9c516c6670c592195dc6c8f20841622b4bcc737a
ali-ulu <3.05066106e+08+ali-ulu@users.noreply.github.com>
2026-08-20 10:14:18 +0000

    refactor: delegate Graph _consolidateEdges to graph-consolidate-edges (#328)

--- graph.js
+++ graph.js
@@ -73,6 +73,7 @@ const { optimize: runGraphOptimize } = require('./lib/graph-optimize');
 const { isCausalRelation: runIsCausalRelation, getCausalRelations: runCausalRelations, getCausalEdges: runCausalEdges } = require('./lib/graph-causal-relation-read');
 const { addEdge: runEdgeWrite } = require('./lib/graph-edge-write');
 const { ensureMutationReceiptFamilySchema: runMutationReceiptFamilySchema } = require('./lib/graph-mutation-receipt-schema');
+const consolidateEdges = require('./lib/graph-consolidate-edges');
 
 class Graph {
   /**
@@ -665,72 +666,7 @@ class Graph {
   }
 
   _consolidateEdges(dryRun = true) {
-    const edges = this._edges;
-    const removed = [];
-    const marked = new Set();
-    const byPair = {};
-
-    for (let i = 0; i < edges.length; i++) {
-      if (edges[i].kistlama) continue;
-      const key = `${edges[i].from}|${edges[i].to}`;
-      if (!byPair[key]) byPair[key] = [];
-      byPair[key].push(i);
-    }
-
-    for (const indices of Object.values(byPair)) {
-      const high = indices.filter(i => edges[i].weight >= 0.5);
-      const low = indices.filter(i => edges[i].weight < 0.3);
-      for (const index of low) {
-        if (high.length > 0) {
-          removed.push({
-            idx: index,
-            edge: edges[index],
-            reason: `low-weight (${edges[index].weight}) superseded by high-weight (${edges[high[0]].weight}) for same pair`,
-          });
-          marked.add(index);
-        }
-      }
-    }
-
-    const byRelation = {};
-    for (let i = 0; i < edges.length; i++) {
-      if (marked.has(i) || edges[i].kistlama) continue;
-      const key = `${edges[i].from}|${edges[i].relation}`;
-      if (!byRelation[key]) byRelation[key] = [];
-      byRelation[key].push(i);
-    }
-
-    for (const indices of Object.values(byRelation)) {
-      const high = indices.filter(i => edges[i].weight >= 0.5);
-      const low = indices.filter(i => edges[i].weight < 0.3);
-      for (const index of low) {
-        if (high.length > 0 && !marked.has(index)) {
-          removed.push({
-            idx: index,
-            edge: edges[index],
-            reason: `low-weight restriction (${edges[index].weight}) \u00e2\u20ac\u201d subject already has high-weight '${edges[index].relation}'`,
-          });
-          marked.add(index);
-        }
-      }
-    }
-
-    if (!dryRun && removed.length > 0) {
-      this._edges = edges.filter((_, index) => !marked.has(index));
-      this._rebuildIndex();
-      try {
-        this.save();
-      } catch (error) {
-        console.error('[Kernel] Graph save hatası:', error.message);
-      }
-    }
-
-    return {
-      dryRun,
-      removed: removed.length,
-      details: removed.map(({ edge, reason }) =>
-        `${edge.from} ? ${edge.to} (${edge.relation}, w:${edge.weight}): ${reason}`),
-    };
+    return consolidateEdges({ edges: this._edges, dryRun, replaceEdges: arr => { this._edges = arr; }, rebuildIndex: () => this._rebuildIndex(), save: () => this.save(), logSaveError: error => { console.error('[Kernel] Graph save hatası:', error.message); } });
   }
 
   getNodes(workspaceId = 'default') {
